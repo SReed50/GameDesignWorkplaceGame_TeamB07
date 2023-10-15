cdcf310216c39dec06910b741ace0dbf33e29ce6
Allen <allenzhufangwang@gmail.com>
2023-10-15 14:19:36 -0400

    Added Timer
    the timer is added, and the script GameHandler is updated!

--- TeamB7MidtermGame/Assets/Scripts/GameHandler.cs
+++ TeamB7MidtermGame/Assets/Scripts/GameHandler.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameHandler : MonoBehaviour
 {
+
+    public float theTimer;
+    public Text timerText;
+    public float startTime = 20;
+
+    public bool isEnd = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        theTimer = startTime;
+        if (isEnd) {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
     }
 
     // Update is called once per frame
@@ -16,6 +28,14 @@ public class GameHandler : MonoBehaviour
     {
         if (Input.GetKey("escape")) {
                  QuitGame();
+        }
+    }
+
+    void FixedUpdate() {
+        theTimer -= Time.deltaTime;
+        timerText.text = "Time: " + Mathf.Floor(theTimer);
+        if ((theTimer <= 0)&&(isEnd == false)) {
+            SceneManager.LoadScene("EndLose");
         }
     }
 
