6da15747c5248430eb3b1ea816c99eded49bf71b
Silas Reed <silas.reed@tufts.edu>
2023-10-14 21:56:20 -0400

    Updated functionality of health bar

--- TeamB7MidtermGame/Assets/Scripts/PlayerHealth.cs
+++ TeamB7MidtermGame/Assets/Scripts/PlayerHealth.cs
@@ -4,29 +4,31 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Display_and_decrease_playerhealth_when_collided_with_enemies_or_objects : MonoBehaviour
 {
-      public float health;
+      public float health = 100f;
       public Slider slider;
       public Text text;
     // Start is called before the first frame update
     void Start()
     {
-    
+        slider.value = health;                           
+        text.text="Health: "+health;
     }
 
     // Update is called once per frame
     void Update()
-    {
-                                                    
-     slider.value=health;                           
-     text.text="Health : "+health;                  
+    {                  
         
     }
                                                     
     void OnCollisionEnter(Collision obj)
     {
                                                     
-        if(obj.gameObject.tag=="Hazard")
-        health=health-10f;                         
+        if (obj.gameObject.tag == "Hazard") {
+            health = health - 10f;
+            slider.value = health;                           
+            text.text = "Health: " + health;
+        }
+                             
     }
  
 }
